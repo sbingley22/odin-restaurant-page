f394d960d3b526847adc424ff01136590302eb53
sbingley22 <sbingley22@gmail.com>
2023-10-18 11:07:35 +0100

    added imgs and styling

--- src/load_page.js
+++ src/load_page.js
@@ -1,3 +1,7 @@
+import dorotaImg from './dorota-semla.jpg'
+import spencerImg from './spencer-davis.jpg'
+import valeriaImg from './valeria-boltneva.jpg'
+
 const loadMainPage = () => {
     const mainContainer = document.createElement("div")
     const headerContainer = document.createElement("div")
@@ -13,10 +17,20 @@ const loadMainPage = () => {
     const restaurantInfo = document.createElement("p")
     restaurantInfo.textContent = 'We provide food for a lifetime of enjoyment. Live well, eat well.'
 
+    const dorotaImgElement = document.createElement("img")
+    dorotaImgElement.src = dorotaImg
+    const spencerImgElement = document.createElement("img")
+    spencerImgElement.src = spencerImg
+    const valeriaImgElement = document.createElement("img")
+    valeriaImgElement.src = valeriaImg
+
     mainContainer.appendChild(headerContainer)
     mainContainer.appendChild(bodyContainer)
     headerContainer.appendChild(logoTitle)
     bodyContainer.appendChild(restaurantInfo)
+    bodyContainer.appendChild(dorotaImgElement)
+    bodyContainer.appendChild(spencerImgElement)
+    bodyContainer.appendChild(valeriaImgElement)
 
     return mainContainer
 } 
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,21 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
